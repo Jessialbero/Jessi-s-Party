b154454689ec270d31c7718b7d59f3889c097307
Jessialbero <jessialbero@gmail.com>
2022-09-01 22:55:19 -0300

    cambio de wording

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <section class="header">
+        <section className="header">
         <GiPartyFlags style={{color: '#9720bf', fontSize: '50px'}} />
         <h1>Jessi´s Party</h1>
           <h3>Este año mi cumple se dividirá en dos celebraciones </h3>
@@ -15,7 +15,7 @@ function App() {
         </div>
                 <a className="btn-bgstroke" href='https://forms.gle/6MLhXb8dgoEc7HRa7'>Juego de Escape </a>
                 <a className="btn-bgstroke" href='https://forms.gle/aGxdPk5Kgbh9McN19'>Che Bonche </a>
-                  <p className='PD'>Por fa selecciona a dónde me acompañarás </p>
+                  <p className='PD'>Por fa selecciona dónde celebrarás conmigo </p>
         </section>
       </header>
     </div>
